9aa52a6ef084d00fab54ce9453835acdec6cb21a
byungchul-chae <abchae0@gmail.com>
2024-01-05 17:14:47 +0900

    post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "[Paper Review] Matrix Compression via Randomized Low Rank and Low Precision Factorization",
+        "excerpt":"Info 2023 NeurIPS에서 발표된 논문. Low Rank Low Precision 알고리즘을 통해 모델, 데이터셋을 효과적으로 압축 Introduction Proposed Algorithm Uniformly Dithered Quantizer 이 논문의 알고리즘에 사용되는 양자화 방식이다. 자세한 설명 Propoed Algorithm : LPLR Direct SVD Qaunt 최적화 문제의 제시 LPLR 알고리즘 알고리즘의 이점 가우시안 행렬의 선택 Approximation Error Analysis Numerical...","categories": ["Paper","Compression"],
+        "tags": ["low rank","compression","neurips","paper review"],
+        "url": "/paper/compression/Matrix-Compression-NeurIPS-2023/",
+        "teaser": null
+      }]
